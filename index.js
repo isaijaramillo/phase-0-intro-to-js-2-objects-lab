ba5c8a86559c285176464f4a57a7008b91dee898
Isai Jaramillo <isai.jaramillo2003@gmail.com>
2023-07-16 14:20:41 -0500

    adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+var employee = { name: "Bob", streetAddress: "123 Main Street" };
+
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+var employee = { name: "Bob", streetAddress: "123 Main Street" };
+
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+var employee = {name: "Bob"};
+
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+var employee = { name: "Bob" };
+
